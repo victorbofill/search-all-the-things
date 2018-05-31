d2238ed17f0e11f1f0b411aa3126eb91e3db5536
victorbofill <victorm.bofill@gmail.com>
2018-05-30 17:50:30 -0700

    lint passes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import styles from './App.css';
+// import styles from './App.css';
 
 
 export default class App extends Component {
   state = {};
 
   render() {
-    return(
+    return (
       <h1>test</h1>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
+/* eslint-env node */
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 
 const buildDir = 'docs';
-const path = `${__dirname}/${buildDir}`
+const path = `${__dirname}/${buildDir}`;
 
 module.exports = {
   entry: './src/index.js',
