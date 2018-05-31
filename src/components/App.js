0038fee48d422b85d0a7f8347c5776f5b4f31270
victorbofill <victorm.bofill@gmail.com>
2018-05-30 17:34:16 -0700

    continued building of react env. still not working

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styles from './App.css';
+
+
+export default class App extends Component {
+  state = {};
+
+  render() {
+    return(
+      <h1>test</h1>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/main.css';
+import App from './components/App';
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
