7a9456a45c1a1494cca7035b721228c8b0334dee
victorbofill <victorm.bofill@gmail.com>
2018-05-30 17:07:26 -0700

    initialization

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project Name
+
+## Overview
+A brief description of the project
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+/* eslint-env node */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
