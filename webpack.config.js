454da7b032b4a8ad7477d8436f8abf3ff2c3d668
victorbofill <victorm.bofill@gmail.com>
2018-05-30 17:20:20 -0700

    webpack config progress; not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
-/* eslint-env node */
-const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+
+const buildDir = 'docs';
+const path = `${__dirname}/${buildDir}`
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path,
+    filename: 'bundle.[hash].js'
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: `./${buildDir}`
   },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(`${path}/bundle.*.js`),
+    new HtmlPlugin({ template: './src/index.html' })
+  ],
   module: {
     rules: [
       {
@@ -24,12 +32,28 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'style-loader'
+            loader: 'style-loader',
+            options: { sourceMap: true }
           },
           {
-            loader: 'css-loader'
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true }
           }
         ]
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 5000 }
+        }
       }
     ]
   }
