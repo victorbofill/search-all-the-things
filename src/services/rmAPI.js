9c593acf27251de365a90a12cf0f314e7dc12a9f
victorbofill <victorm.bofill@gmail.com>
2018-05-31 16:58:58 -0700

    created search methods for location and episode in the API code

--- src/services/rmAPI.js
+++ src/services/rmAPI.js
@@ -1,5 +1,7 @@
 const BASE_URL = 'https://rickandmortyapi.com/api/';
 const CHARACTERS_URL = 'character/';
+const EPISODES_URL = 'episode/';
+const LOCATION_URL = 'location/';
 
 const throwJson = json => { throw json ; };
 const get = url => fetch(url)
@@ -10,4 +12,18 @@ export function searchCharacters({ character }, { page = 1 }) {
   const paging = `page=${page}`;
 
   return  get(`${BASE_URL}${CHARACTERS_URL}${search}&${paging}`);
+}
+
+export function searchEpisodes({ episode }, { page = 1 }) {
+  const search = `?name=${episode}`;
+  const paging = `page=${page}`;
+
+  return get(`${BASE_URL}${EPISODES_URL}${search}&${paging}`);
+}
+
+export function searchLocations({ location }, { page = 1 }) {
+  const search = `?name=${location}`;
+  const paging = `page=${page}`;
+
+  return get(`${BASE_URL}${LOCATION_URL}${search}&${paging}`);
 }
