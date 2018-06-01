bb78aedbe57cdf5671e5e95a0367eae9abec81d5
victorbofill <victorm.bofill@gmail.com>
2018-06-01 12:36:53 -0700

    final css styling

--- src/components/episodes/Episode.js
+++ src/components/episodes/Episode.js
@@ -3,11 +3,12 @@ import styles from './Episode.css';
 
 export default class Episodes extends Component {
   render() {
-    const { name, air_date } = this.props.episode;
+    const { name, air_date, episode } = this.props.episode;
 
     return (
       <li className={styles['results-li']}>
-        <h2>Name: {name}</h2>
+        <h2>{name}</h2>
+        <p>{episode}</p>
         <p>Air Date: {air_date}</p>
       </li>
     );
