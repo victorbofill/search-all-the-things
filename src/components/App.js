e4c59d76896e4be12653e958b9ab29e060c4489e
victorbofill <victorm.bofill@gmail.com>
2018-05-31 17:32:50 -0700

    minor tweaks

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-// import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
-import logo from './logo.jpg';
 import Paging from './Paging';
 import { searchCharacters } from '../services/rmAPI';
-// import { searchCharacters, searchEpisodes, searchLocations } from '../services/rmAPI';
 import SearchCharacters from './SearchCharacters';
 import CharacterResults from './CharacterResults';
+import logo from './logo.jpg';
+import './App.css';
+// import { searchCharacters, searchEpisodes, searchLocations } from '../services/rmAPI';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+// TODO:
+// Clean up search code; remove 'character' from everything that doesn't need it
+// Copy/create search bars for locations and episodes
+// Create router to switch between search criteria, with nav bar
+// Search functionality for all three criteria
 
 export default class App extends Component {
   state = {
